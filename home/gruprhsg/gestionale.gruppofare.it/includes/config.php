507a615d4ad790ba7e1b0e895d5d10d884ad2632
Samuga25 <samuelegallozzi@outlook.it>
2026-04-22 09:56:15 -0400

    rimossa cartella home

--- home/gruprhsg/gestionale.gruppofare.it/includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('APP_URL', 'https://gestionale.gruppofare.it');
-
-function secureSession() {
-    if (empty($_SESSION['_initiated'])) {
-        session_regenerate_id(true);
-        $_SESSION['_initiated'] = true;
-    }
-}
--- home/gruprhsg/gestionale.gruppofare.it/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-// Reindirizza sempre a login.php
-header("Location: login.php");
-exit;
-?>
